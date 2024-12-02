5812605a1fa3f8d6c0121c8af1111cb71effee1c
Kolos Foltányi <kolos65@gmail.com>
2024-12-02 11:49:27 +0100

    feat: add concurrency related tests

--- Tests/MockableTests/BuildTests.swift
+++ Tests/MockableTests/BuildTests.swift
@@ -8,26 +8,24 @@
 import Mockable
 
 @Mockable
-protocol TestProtocol: Actor, Sendable where Item2: Identifiable {
-
-    // MARK: Associated Types
-
+protocol TestAssociatedTypes where Item2: Identifiable {
     associatedtype Item1
     associatedtype Item2: Equatable, Hashable
     associatedtype Item3 where Item3: Equatable, Item3: Hashable
-
     func foo(item1: Item1) -> Item1
     func foo(item2: Item2) -> Item2
     func foo(item3: Item3) -> Item3
+}
 
-    // MARK: Exotic Parameters
-
+@Mockable
+protocol TestExoticParameters {
     func modifyValue(_ value: inout Int)
     func printValues(_ values: Int...)
     func execute(operation: @escaping () throws -> Void)
+}
 
-    // MARK: Function Effects
-
+@Mockable
+protocol TestFunctionEffects {
     func canThrowError() throws
     func returnsAndThrows() throws -> String
     func call(operation: @escaping () throws -> Void) rethrows
@@ -35,26 +33,29 @@ protocol TestProtocol: Actor, Sendable where Item2: Identifiable {
     func asyncThrowingFunction() async throws
     func asyncParamFunction(param: @escaping () async throws -> Void) async
     nonisolated func nonisolatedFunction() async
+}
 
-    // MARK: Generic Functions
-
+@Mockable
+protocol TestGenericFunctions {
     func foo<T>(item: (Array<[(Set<T>, String)]>, Int))
     func genericFunc<T, V>(item: T) -> V
+    func getInts() -> any Collection<Int>
     func method<T: Hashable, E, C, I>(
         prop1: T, prop2: E, prop3: C, prop4: I
     ) where E: Equatable, E: Hashable, C: Codable
-    func getInts() -> any Collection<Int>
+}
 
-    // MARK: Name Collision
-
+@Mockable
+protocol TestNameCollisions {
     func fetchData(for name: Int) -> String
     func fetchData(for name: String) -> String
     func fetchData(forA name: String) -> String
     func fetchData(forB name: String) -> String
     func `repeat`(param: Bool) -> Int
+}
 
-    // MARK: Property Requirements
-
+@Mockable
+protocol TestPropertyRequirements {
     var computedInt: Int { get }
     var computedString: String { get }
     var mutableInt: Int { get set }
@@ -63,21 +64,46 @@ protocol TestProtocol: Actor, Sendable where Item2: Identifiable {
     var asyncProperty: String { get async }
     var asyncThrowingProperty: String { get async throws }
     nonisolated var nonisolatedProperty: String { get set }
+}
 
-    // MARK: Init
-
+@Mockable
+protocol TestInitRequirements {
     init?() async throws
     init(index: Int)
     init(name value: String, index: Int)
+}
 
-    // MARK: Attributes
-
+@Mockable
+protocol TestAttributes {
     @available(iOS 16, *)
     init(attributed: String)
-
     @available(iOS 16, *)
     var attributedProp: Int { get }
-
     @available(iOS 16, *)
     func attributedTest()
+}
+
+#if canImport(Foundation)
+import Foundation
+
+@Mockable
+protocol TestNSObject: NSObjectProtocol {
+    func foo(param: Int) -> String
+}
+#endif
+
+@Mockable
+protocol TestActorConformance: Actor {
+    func foo(param: Int) -> String
+}
+
+@Mockable
+@MainActor
+protocol TestGlobalActor {
+    func foo(param: Int) -> String
+}
+
+@Mockable
+protocol TestSendable: Sendable {
+    func foo(param: Int) -> String
 }
