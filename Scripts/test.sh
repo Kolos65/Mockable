89b0d78765c34d3be3d2334391631ab6f12c6c07
Pedro <pedro@pepicrft.me>
2025-01-30 17:09:50 +0100

    chore: run CI tests on Linux with Swift 5.9 and 6.0

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -2,5 +2,21 @@
 
 set -eo pipefail
 
-swift build
-swift test
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ROOT_DIR="$(dirname "$SCRIPT_DIR")"
+source $ROOT_DIR/Scripts/utils.sh
+
+# When --vm is passed, the build and test commands are executed in a virtualized container.
+if [[ " $* " == *" --vm "* ]]; then
+    CONTAINER_RUNTIME=$(get_container_runtime)
+    $CONTAINER_RUNTIME run --rm \
+        --volume "$ROOT_DIR:/package" \
+        --workdir "/package" \
+        -e MOCKABLE_TEST=$MOCKABLE_TEST \
+        swiftlang/swift:nightly-6.1-focal \
+        /bin/bash -c \
+        "swift build --build-path ./.build/linux"
+else
+    swift build --package-path "$ROOT_DIR"
+    swift test --package-path "$ROOT_DIR"
+fi
