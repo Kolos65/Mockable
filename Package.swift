0a822d53fa7516abe659b44a9c508b8eb9b666bd
Scott Hoyt <scottrhoyt@gmail.com>
2025-09-29 20:53:19 -0700

    feat: update to swift-syntax 602.0.0

--- Package.swift
+++ Package.swift
@@ -53,7 +53,7 @@ let package = Package(
         )
     ],
     dependencies: devDependencies + [
-        .package(url: "https://github.com/swiftlang/swift-syntax.git", "509.0.0"..<"602.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", "509.0.0"..<"603.0.0"),
         .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", .upToNextMajor(from: "1.6.1"))
     ],
     targets: devTargets + [
