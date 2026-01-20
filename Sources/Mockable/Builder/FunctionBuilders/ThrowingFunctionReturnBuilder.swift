550c6984c0f39d4e6d00f3d9a21b8240b7bdca31
Kolos Foltányi <kolos65@gmail.com>
2026-01-20 08:39:42 +0100

    doc: update documentation with willHandle

--- Sources/Mockable/Builder/FunctionBuilders/ThrowingFunctionReturnBuilder.swift
+++ Sources/Mockable/Builder/FunctionBuilders/ThrowingFunctionReturnBuilder.swift
@@ -66,7 +66,7 @@ public struct ThrowingFunctionReturnBuilder<
         return .init(mocker: mocker)
     }
 
-    /// Registers a result type that automatically emits a value when successful or throws a failure
+    /// Registers a result type that automatically emits a value when successful or throws a failure.
     ///
     /// - Parameter result: The result type wrapping a value or error.
     /// - Returns:  The parent builder, used for chaining additional specifications.
